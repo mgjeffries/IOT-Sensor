1a7a8737a2e5c74a8088529205182f01d508e5e6
mgjeffries <3511151@gmail.com>
2016-12-31 18:56:30 -0600

    Counter added

--- FlaskHelloWorld.py
+++ FlaskHelloWorld.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
+global counter
+counter = 0
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def counterPage():
+    global counter
+    counter = counter + 1
+    return 'Hello, world!'
+
+
+@app.route('/asfd')
+def logPage():
+    with open("log.txt", "a") as myfile:
+        myfile.write( str(counter) + "\n" )
+    return str(counter)
+
+counterPage()
+logPage()
